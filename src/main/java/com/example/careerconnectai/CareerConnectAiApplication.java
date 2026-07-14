f105d2b17733f3e75d71dbd5c1ad91448318f0fe
Bhavishyata Yadav <your-github-raobhavi17@gmail.com>
2026-07-14 16:01:12 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/careerconnectai/CareerConnectAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.careerconnectai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerConnectAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerConnectAiApplication.class, args);
+    }
+
+}
